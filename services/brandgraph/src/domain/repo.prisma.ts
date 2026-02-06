77cbd9c942b5b0c99f178d1221e975eb326b2a66
Andre <darksiedllc@gmail.com>
2026-02-06 14:31:42 -0800

    chore(brandgraph): remove unused Prisma import

--- services/brandgraph/src/domain/repo.prisma.ts
+++ services/brandgraph/src/domain/repo.prisma.ts
@@ -1,4 +1,4 @@
-import { Prisma, type PrismaClient } from "../generated/prisma/index.js";
+import type { PrismaClient } from "../generated/prisma/index.js";
 import { GraphQueryOptions, GraphSnapshot, GraphSnapshotList } from './graph';
 import type { ArtifactLink, Brand, BrandGraphRepo, GraphEvent } from "./repo.js";
 
